58d4893e8c27c1847e3c3d0f9cf2208fa518914a
Morel504 <ianmorelvalladares2003@gmail.com>
2023-01-26 21:01:01 -0600

    Tercera Clase
    Opciones de Caracteres

---
+++ IParcial/IParcial/Ejercicio3.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace IParcial
+{
+    partial class Ejercicio3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CadenaTextBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EjecutarButton1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LongitudTextBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PrimerCaracterTextBox = new System.Windows.Forms.TextBox();
+            this.UltimoCaracterTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.MayusculasTextBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MinusculasTextBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ReemplazarTextBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CadenaTextBox1
+            // 
+            this.CadenaTextBox1.Location = new System.Drawing.Point(193, 46);
+            this.CadenaTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.CadenaTextBox1.Name = "CadenaTextBox1";
+            this.CadenaTextBox1.Size = new System.Drawing.Size(512, 26);
+            this.CadenaTextBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Cadena:";
+            // 
+            // EjecutarButton1
+            // 
+            this.EjecutarButton1.Location = new System.Drawing.Point(193, 96);
+            this.EjecutarButton1.Name = "EjecutarButton1";
+            this.EjecutarButton1.Size = new System.Drawing.Size(115, 29);
+            this.EjecutarButton1.TabIndex = 2;
+            this.EjecutarButton1.Text = "Ejecutar";
+            this.EjecutarButton1.UseVisualStyleBackColor = true;
+            this.EjecutarButton1.Click += new System.EventHandler(this.EjecutarButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(90, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Longitud";
+            // 
+            // LongitudTextBox1
+            // 
+            this.LongitudTextBox1.Location = new System.Drawing.Point(195, 137);
+            this.LongitudTextBox1.Name = "LongitudTextBox1";
+            this.LongitudTextBox1.Size = new System.Drawing.Size(133, 26);
+            this.LongitudTextBox1.TabIndex = 4;
+            this.LongitudTextBox1.TextChanged += new System.EventHandler(this.LongitudTextBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Primer Caracter";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // PrimerCaracterTextBox
+            // 
+            this.PrimerCaracterTextBox.Location = new System.Drawing.Point(193, 188);
+            this.PrimerCaracterTextBox.Name = "PrimerCaracterTextBox";
+            this.PrimerCaracterTextBox.Size = new System.Drawing.Size(133, 26);
+            this.PrimerCaracterTextBox.TabIndex = 6;
+            // 
+            // UltimoCaracterTextBox
+            // 
+            this.UltimoCaracterTextBox.Location = new System.Drawing.Point(193, 237);
+            this.UltimoCaracterTextBox.Name = "UltimoCaracterTextBox";
+            this.UltimoCaracterTextBox.Size = new System.Drawing.Size(133, 26);
+            this.UltimoCaracterTextBox.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 237);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Ultimo Caracter";
+            // 
+            // MayusculasTextBox1
+            // 
+            this.MayusculasTextBox1.Location = new System.Drawing.Point(193, 288);
+            this.MayusculasTextBox1.Name = "MayusculasTextBox1";
+            this.MayusculasTextBox1.Size = new System.Drawing.Size(133, 26);
+            this.MayusculasTextBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Mayusculas";
+            // 
+            // MinusculasTextBox1
+            // 
+            this.MinusculasTextBox1.Location = new System.Drawing.Point(193, 339);
+            this.MinusculasTextBox1.Name = "MinusculasTextBox1";
+            this.MinusculasTextBox1.Size = new System.Drawing.Size(133, 26);
+            this.MinusculasTextBox1.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(40, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Minisculas";
+            // 
+            // ReemplazarTextBox1
+            // 
+            this.ReemplazarTextBox1.Location = new System.Drawing.Point(193, 387);
+            this.ReemplazarTextBox1.Name = "ReemplazarTextBox1";
+            this.ReemplazarTextBox1.Size = new System.Drawing.Size(133, 26);
+            this.ReemplazarTextBox1.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 387);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(86, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Reemplzar";
+            // 
+            // Ejercicio3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 503);
+            this.Controls.Add(this.ReemplazarTextBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.MinusculasTextBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.MayusculasTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.UltimoCaracterTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PrimerCaracterTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.LongitudTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EjecutarButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CadenaTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Ejercicio3";
+            this.Text = "Ejercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox CadenaTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button EjecutarButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LongitudTextBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PrimerCaracterTextBox;
+        private System.Windows.Forms.TextBox UltimoCaracterTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MayusculasTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MinusculasTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox ReemplazarTextBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ IParcial/IParcial/Ejercicio3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IParcial
+{
+    public partial class Ejercicio3 : Form
+    {
+        public Ejercicio3()
+        {
+            InitializeComponent();
+        }
+
+        private void LongitudTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EjecutarButton1_Click(object sender, EventArgs e)
+        {
+            string cadena = CadenaTextBox1.Text;
+            LongitudTextBox1.Text = cadena.Length.ToString();
+
+            PrimerCaracterTextBox.Text = cadena.Substring(0,1);
+            UltimoCaracterTextBox.Text = cadena.Substring(cadena.Length - 1, 1);
+            
+            MayusculasTextBox1.Text = cadena.ToUpper();
+            MinusculasTextBox1.Text = cadena.ToLower();
+            ReemplazarTextBox1.Text = cadena.Replace("Ian Morel", "MUY GUAPOOOOO");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
