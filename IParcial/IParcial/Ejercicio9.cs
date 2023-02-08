61ea35d9d46dca96223b2df376a90c5c8cd9e56b
Morel504 <ianmorelvalladares2003@gmail.com>
2023-02-07 21:00:34 -0600

    Clase 9
    Comienzo de POO

---
+++ IParcial/IParcial/Ejercicio9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using POO;
+
+namespace IParcial
+{
+    public partial class Ejercicio9 : Form
+    {
+        public Ejercicio9()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Cochce miCoche = new Cochce();
+            Cochce miCoche2= new Cochce("Toyota", "22R");
+
+
+
+        }
+    }
+}
---
+++ IParcial/POO/Cochce.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Cochce
+    {
+        //Atrinutos
+        private string marca;
+        private string modelo;
+        private decimal precio;
+
+        //Propiedades
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public string Modelo
+        {
+            get => modelo; set => modelo = value;
+        }
+        public decimal Precio { get;set }
+
+        public int Kilometros { get;set; }
+
+
+
+    }
+}
